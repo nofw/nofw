195d8fbd47ab152c14bb5d82c43d5d747f30f05d
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-02-04 20:43:38 +0100

    Add debug parameter override

--- web/app.php
+++ web/app.php
@@ -2,7 +2,17 @@
 
 require __DIR__.'/../app/bootstrap.php';
 
-$request    = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+$request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+
+// Override debug parameter
+$query = $request->getQueryParams();
+if (isset($query['debug'])) {
+    if ('false' === $query['debug']) {
+        $container->set('debug', false);
+    } elseif ('true' === $query['debug']) {
+        $container->set('debug', true);
+    }
+}
 
 /** @var \Middlewares\Utils\Dispatcher $dispatcher */
 $dispatcher = $container->get('dispatcher');
