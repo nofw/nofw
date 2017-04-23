d48716e681145f38cbd1e5bf2bff238da2fc48e3
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-04-23 21:25:25 +0200

    Remove debug override

--- web/app.php
+++ web/app.php
@@ -4,16 +4,6 @@ require __DIR__.'/../app/bootstrap.php';
 
 $request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
 
-// Override debug parameter
-$query = $request->getQueryParams();
-if (isset($query['debug'])) {
-    if ('false' === $query['debug']) {
-        $container->set('debug', false);
-    } elseif ('true' === $query['debug']) {
-        $container->set('debug', true);
-    }
-}
-
 /** @var \Middlewares\Utils\Dispatcher $dispatcher */
 $dispatcher = $container->get('dispatcher');
 
